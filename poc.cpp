22a95f731deea7a5d40e7e503dfdca08aeaffbed
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-15 11:01:35 +0100

    some input and movement

--- poc.cpp
+++ poc.cpp
@@ -1,16 +1,37 @@
 #pragma leco app
 
 import casein;
+import dotz;
+import input;
 import quack;
+import sitime;
+
+static dotz::vec2 player_pos {};
+static sitime::stopwatch timer {};
 
 static void update_data() {
   quack::donald::data([](auto *& i) {
     *i++ = {
-      .position = { 0, 0 },
+      .position = player_pos,
       .size = { 1, 1 },
       .colour = { 1, 0, 0, 1 },
     };
   });
+}
+
+static void move_player(float dt) {
+  auto d = input::left_stick();
+  if (dotz::length(d) < 0.001) return;
+
+  player_pos = player_pos + d * dt * 10.0;
+}
+
+static void repaint() {
+  float dt = timer.millis() / 1000.f;
+  timer = {};
+
+  move_player(dt);
+  update_data();
 }
 
 struct init {
@@ -22,13 +43,15 @@ struct init {
     max_quads(1024);
 
     clear_colour({ 0, 0, 0, 1 });
-    // atlas("atlas.png");
 
     push_constants({
-        .grid_pos = { 2, 2 },
-        .grid_size = { 4, 4 },
+        .grid_pos = { 0, -6 },
+        .grid_size = { 16, 16 },
     });
 
-    update_data();
+    input::setup_defaults();
+
+    repaint();
+    handle(REPAINT, repaint);
   }
 } i;
