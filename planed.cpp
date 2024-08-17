41aea32237deafac3c416d69e6cc7a0739756c0a
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-17 12:51:40 +0100

    brush selection

--- planed.cpp
+++ planed.cpp
@@ -6,12 +6,20 @@ import dotz;
 import quack;
 
 static dotz::ivec2 g_cursor{};
+static dotz::ivec2 g_brush{ 1, 2 };
 
 static void update_data(quack::instance *& i) {
   *i++ = {
     .position = g_cursor - 0.1f,
     .size = { 1.2f },
     .colour = { 1, 0, 0, 1 },
+  };
+  *i++ = {
+    .position = g_cursor,
+    .size = { 1 },
+    .uv0 = (g_brush - 1) / 16.f,
+    .uv1 = (g_brush + 2) / 16.f,
+    .multiplier = { 1 },
   };
 }
 static void update_data() { quack::donald::data(::update_data); }
@@ -20,6 +28,13 @@ static constexpr auto move(int dx, int dy) {
   return [=] {
     auto c = g_cursor + dotz::ivec2 { dx, dy };
     g_cursor = dotz::clamp(c, { 0 }, { 31 });
+    update_data();
+  };
+}
+static constexpr auto brush(unsigned n) {
+  return [=] {
+    g_brush.x = 1 + 3 * n;
+    g_brush.y = 2;
     update_data();
   };
 }
@@ -43,5 +58,8 @@ struct init {
     handle(KEY_DOWN, K_DOWN, move(0, 1));
     handle(KEY_DOWN, K_LEFT, move(-1, 0));
     handle(KEY_DOWN, K_RIGHT, move(1, 0));
+
+    handle(KEY_DOWN, K_1, brush(0));
+    handle(KEY_DOWN, K_2, brush(1));
   }
 } i;
