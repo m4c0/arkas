23bc9b3d76b392d73d6721ec23389ab1ea9a7fa4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-26 13:32:15 +0100

    pixel-almost-perfect collision

--- poc-collision.cpp
+++ poc-collision.cpp
@@ -6,10 +6,18 @@ import pixed;
 import quack;
 import ships;
 import silog;
+import traits;
+
+using namespace traits::ints;
 
 static constexpr const dotz::vec2 enemy_pos { -2, -4 };
 static dotz::vec2 g_player_pos { 2, 4 };
 static bool g_blink {};
+
+struct mask {
+  unsigned rows[16] {};
+};
+static mask g_masks[16][16] {};
 
 static bool collided() {
   auto bs = g_player_pos;
@@ -21,7 +29,23 @@ static bool collided() {
   if (ee.x < bs.x || es.x > be.x) return false;
   if (ee.y < bs.y || es.y > be.y) return false;
 
-  return true;
+  const auto & e_msk = g_masks[2][0];
+  const auto & b_msk = g_masks[1][0];
+
+  int dx = (es.x - bs.x) * 16;
+  // Should not happen, but FP precision am I right?
+  if (dx <= -16 || dx >= 16) return false;
+
+  for (int y = 0; y < 16; y++) {
+    int dy = es.y * 16 + y - bs.y * 16;
+    if (dy < 0 || dy >= 16) continue;
+
+    uint64_t em = e_msk.rows[y] << 16;
+    uint64_t bm = b_msk.rows[dy] << (16 + dx);
+    if ((em & bm) != 0) return true;
+  }
+
+  return false;
 }
 
 static void setup_buffer() {
@@ -33,10 +57,6 @@ static void setup_buffer() {
 
 static void mouse_move() { g_player_pos = ships::mouse_pos() - 0.5f; }
 
-struct mask {
-  unsigned rows[16] {};
-};
-static mask g_masks[16][16] {};
 static void create_bitmaps(const pixed::context & ctx) {
   for (auto y = 0, p = 0; y < ctx.h; y++) {
     for (auto x = 0; x < ctx.w; x++, p++) {
